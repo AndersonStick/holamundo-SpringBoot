4d882bbce16ea8e1f649f9881dbebb8c45b213ec
Anderson Barrera <andersonbar10@gmail.com>
2023-10-16 21:13:06 -0500

    Se crea el proyecto de hola mundo en SpringBoot

---
+++ src/main/java/com/example/holmundo/HolmundoApplication.java
@@ -0,0 +1,13 @@
+package com.example.holmundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolmundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolmundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/holmundo/HolmundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holmundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolmundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
